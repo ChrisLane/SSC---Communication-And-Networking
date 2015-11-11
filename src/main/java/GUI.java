fbda0248df34dd8f4628ac36e2102636921896ae
ChrisLane <ChrisLane@users.noreply.github.com>
2015-11-11 18:00:16 +0000

    Add file attachments and search functionality

--- src/main/java/GUI.java
+++ src/main/java/GUI.java
@@ -1,14 +1,21 @@
-import javax.mail.Flags;
-import javax.mail.Folder;
-import javax.mail.Message;
-import javax.mail.MessagingException;
+import Display.ClientView;
+import Email.Credentials;
+import Email.GmailClient;
+
+import javax.mail.*;
+import javax.mail.search.SearchTerm;
 import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class GUI {
     private GmailClient gmail;
     private ClientView view;
 
     private Folder folder;
+
+    private File attachment;
 
     private JButton loginButton;
     private JPanel viewMail;
@@ -24,6 +31,9 @@ public class GUI {
     private JTextArea emailBody;
     private JSplitPane splitPane;
     private JButton sendEmailButton;
+    private JButton attachFileButton;
+    private JTextField searchTextField;
+    private JButton runSearchButton;
 
     private GUI() {
         loginButton.addActionListener(e -> {
@@ -69,11 +79,14 @@ public class GUI {
                 Message[] messages = gmail.getMail(folder);
                 for (Message message : messages) {
                     try {
-                        if (messageJList.getSelectedValue().equals(message.getSubject() + " - Seen: " + true) ||
-                                messageJList.getSelectedValue().equals(message.getSubject() + " - Seen: " + false)) {
+                        String selectedSubject = messageJList.getSelectedValue()
+                                .replaceAll(" - READ", "");
+                        String messageSubject = message.getSubject();
+                        if (selectedSubject.equals(messageSubject)) {
 
                             view.printMessage(message, messageJList);
                             message.setFlag(Flags.Flag.SEEN, true);
+                            break;
                         }
                     } catch (MessagingException e1) {
                         e1.printStackTrace();
@@ -88,12 +101,54 @@ public class GUI {
             String subject = emailSubject.getText();
             String message = emailBody.getText();
 
-            gmail.sendMessage(to, cc, subject, message);
+            gmail.sendMessage(to, cc, subject, message, attachment);
 
             emailTo.setText("");
             emailCC.setText("");
             emailSubject.setText("");
             emailBody.setText("");
+            attachment = null;
+        });
+
+        attachFileButton.addActionListener(e -> {
+            JFileChooser fileSelect = new JFileChooser();
+            fileSelect.showOpenDialog(splitPane);
+
+            attachment = fileSelect.getSelectedFile();
+        });
+
+        runSearchButton.addActionListener(e -> {
+            Message[] messages = gmail.getMail(folder);
+            String searchTerm = searchTextField.getText();
+
+            SearchTerm search = new SearchTerm() {
+                @Override
+                public boolean match(Message message) {
+                    try {
+                        if (message.getSubject().contains(searchTerm) || message.getContent().toString().contains(searchTerm)) {
+                            return true;
+                        }
+                    } catch (MessagingException | IOException e1) {
+                        e1.printStackTrace();
+                    }
+                    return false;
+                }
+            };
+
+            ArrayList<Message> matchStore = new ArrayList<>();
+            for (Message message : messages) {
+                try {
+                    if (message.match(search)) {
+                        matchStore.add(message);
+                    }
+                } catch (MessagingException e1) {
+                    e1.printStackTrace();
+                }
+            }
+            Message[] matchedMessages = new Message[matchStore.size()];
+            matchStore.toArray(matchedMessages);
+
+            view.printSubjects(matchedMessages, messageJList);
         });
     }
 
